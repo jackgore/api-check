3819a5fa9d05ec18b68a80a2308a5bca4114ba01
Jonathon Gore <jackgore@hotmail.ca>
2018-06-12 17:23:09 -0400

    Showing run statistics after tests are ran

--- printer/printer.go
+++ printer/printer.go
@@ -14,6 +14,12 @@ func succeededText(succeeded bool) string {
 	return "failed"
 }
 
+func printStats(successes, failures, errors int) {
+	total := successes + failures + errors
+
+	fmt.Printf("\n%v tests ran. %v successful. %v failures. %v errors.\n", total, successes, failures, errors)
+}
+
 func printReport(report runner.RunReport) {
 	fmt.Printf("API Check Test for: %v%v %v\n", report.Test.Hostname,
 		report.Test.Endpoint, succeededText(report.Successful))
@@ -23,7 +29,21 @@ func printReport(report runner.RunReport) {
 }
 
 func PrintReports(reports []runner.RunReport) {
+	successes := 0
+	failures := 0
+	errors := 0
+
 	for _, report := range reports {
 		printReport(report)
+
+		if report.Error != nil {
+			errors++
+		} else if report.Successful {
+			successes++
+		} else {
+			failures++
+		}
 	}
+
+	printStats(successes, failures, errors)
 }
