db3803faae3805dc7c890b470352706621c4ac23
Jack Gore <jgore@uwaterloo.ca>
2019-02-22 18:35:16 -0800

    Simplified testing logic

--- builder/generate_test.go
+++ builder/generate_test.go
@@ -21,35 +21,31 @@ var createSkeletonFileTests = []struct {
 func TestCreateSkeletonFile(t *testing.T) {
 	for _, test := range createSkeletonFileTests {
 		filename, err := CreateSkeletonFile(test.prefix)
+        if (err != nil && test.err == nil) || (err == nil && test.err != nil) ||
+            (err != nil && err.Error() != test.err.Error()) {
+          t.Errorf("Expected error: %v but received: %v", test.err, err)
+          continue
+        }
 
-		if test.err != nil {
-			// Case where we expect an error from the test.
-			if err == nil {
-				t.Errorf("Expected test to fail with error: %v but got nil", test.err)
-			} else if err.Error() != test.err.Error() {
-				t.Errorf("Expected test to fail with error: %v but got: %v", test.err, err)
-			}
-		} else if test.err == nil {
-			// Case where we expect the test to succeed.
-			if err != nil {
-				t.Errorf("Expected test to succeed but failed with error: %v", err)
-			} else {
-				// Make sure the contents of the file match those expected
-				contents, err := ioutil.ReadFile("unit-test.ac.json")
-				if err != nil {
-					t.Errorf("Unable to complete test: %v", err)
-				}
-				expectedContents, _ := JSONSkeleton()
-				if !reflect.DeepEqual(contents, expectedContents) {
-					t.Errorf("Found mismatching results when comparing skeleton contents")
-				}
-				// Make sure the file name that was created is as expected.
-				if test.filename != filename {
-					t.Errorf("received unexpected filename - expected: %v received: %v", test.filename, filename)
-				}
-			}
-		}
+        if test.filename != filename {
+            t.Errorf("Expected filename: %v but received: %v", test.filename, filename)
+            continue
+        }
 
-		os.Remove("unit-test.ac.json")
+        // If we expect the test to succeed check the contents of the file.
+        if test.err == nil {
+            contents, err := ioutil.ReadFile("unit-test.ac.json")
+            if err != nil {
+                t.Errorf("Unable to complete test: %v", err)
+                continue
+            }
+
+            expectedContents, _ := JSONSkeleton()
+            if !reflect.DeepEqual(contents, expectedContents) {
+                t.Errorf("Found mismatching results when comparing skeleton contents")
+            }
+        }
+
+		os.Remove(filename)
 	}
 }
