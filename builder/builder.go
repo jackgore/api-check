35539c241f0a21eed75bbb388c7992fa7333f909
Jack Gore <jgore@uwaterloo.ca>
2019-04-07 13:58:03 -0700

    Updated comments in builder to conform to go standards

--- builder/builder.go
+++ builder/builder.go
@@ -1,10 +1,13 @@
 package builder
 
+// Cookie represents a cookie that will be sent to the server for an APITest.
+// This would typically be used as an authentication method.
 type Cookie struct {
 	Name  string `json:"name"`
 	Value string `json:"value"`
 }
 
+// APITest represents a single test that is to be ran be api-check.
 type APITest struct {
 	Description string      `json:"description"`
 	Method      string      `json:"method"`
@@ -14,6 +17,8 @@ type APITest struct {
 	Response    APIResponse `json:"response"`
 }
 
+// APIRequest describes the HTTP request that will be sent by api-check while
+// performing an HTTP request.
 type APIRequest struct {
 	Body        string            `json:"body"`
 	Headers     map[string]string `json:"headers"`
@@ -22,6 +27,8 @@ type APIRequest struct {
 	Cookies     []Cookie          `json:"cookies,omitempty"`
 }
 
+// APIResponse describes that api-check will expect from the server following
+// running an APITest.
 type APIResponse struct {
 	Body       string            `json:"body"`
 	Headers    map[string]string `json:"headers"`
